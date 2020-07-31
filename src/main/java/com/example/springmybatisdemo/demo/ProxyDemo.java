9e722133446b0a66ac62663acb56bb3d399963a4
Just-EP <lovelive7185@outlook.com>
2020-07-31 18:34:38 +0800

    update commit

--- src/main/java/com/example/springmybatisdemo/demo/ProxyDemo.java
+++ src/main/java/com/example/springmybatisdemo/demo/ProxyDemo.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class ProxyDemo {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 6; i++) {
             test();
         }
     }
